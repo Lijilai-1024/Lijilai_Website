0a403dc9567548e2878fe37d705b7e2dba9ab4f2
Lijilai-1024 <lijilai021024@163.com>
2022-08-16 12:04:53 +0800

    加入了菜单栏折叠功能与响应式折叠

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
 <el-container>
-  <el-aside class="menu" width="200px">
-    <el-button class="CollapseButton" type="primary" @click="ChangeCollapse()" circle icon="el-icon-search"></el-button>
+  <el-aside width="auto">
+    <div class="ButtonBox">
+      <el-button class="CollapseButton" type="primary" @click="ChangeCollapse()" circle>
+            <el-icon>
+                <component :is="CollapseIcon" />
+              </el-icon>
+      </el-button>
+    </div>
     <el-menu
-          default-active="1" 
+          default-active="/" 
+          class="menu"
           router
           :collapse="isCollapse"
           active-text-color="#66ccff"
         >
           <el-menu-item index="/">
+            <el-icon><HomeFilled /></el-icon>
             <span>主页</span>
           </el-menu-item >
           <el-menu-item index="/ShowAcgImage">
+            <el-icon><PictureFilled /></el-icon>
             <span>来点图图</span>
           </el-menu-item >
         </el-menu>
@@ -33,7 +42,6 @@
   color: #2c3e50;
 }
 .el-aside {
-  position: relative;
   span {
     font-weight: bold;
     font-size:16px;
@@ -41,23 +49,54 @@
     text-decoration: none;
   }
 }
+.ButtonBox{
+  position:relative;
+  height:40px;
+}
 .CollapseButton{
   position:absolute;
   left: 0;
-  bottom:10px;
+}
+.menu:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
 }
 </style>
 <script>
 export default {
   data(){
     return{
-      isCollapse:false,
+      isCollapse:true,
+      CollapseIcon:"ArrowRightBold",
+      isMobile:true,
     };
   },
   methods:{
     ChangeCollapse() {
       this.isCollapse = !this.isCollapse;
+      this.CollapseIcon = this.isCollapse ? "ArrowRightBold" : "ArrowLeftBold";
     },
+    ReChangeCollapse(val) {
+      if(val === false)
+      {
+        this.isCollapse = false;
+        this.CollapseIcon = "ArrowLeftBold";
+      }
+      if(val === true)
+      {
+        this.isCollapse = true;
+        this.CollapseIcon = "ArrowRightBold";
+      }
+    },
+    listenScreen() {
+      let screenWidth = document.body.clientWidth;	//初始化判断
+      if (screenWidth < 500) this.isMobile = true;
+      else this.isMobile = false;
+    },
+  },
+  mounted(){
+    this.listenScreen();
+    this.ReChangeCollapse(this.isMobile);
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,17 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ELementPlusIcons from '@element-plus/icons-vue'
 const app = createApp(App);
 app.config.globalProperties.$BaseUrl = 'http://127.0.0.1:8000/';
 app.use(store);
 app.use(router);
 app.use(VueAxios, axios);
 app.use(ElementPlus);
+for (const iconName in ELementPlusIcons) {
+    if (Reflect.has(ELementPlusIcons, iconName)) {
+        const item = ELementPlusIcons[iconName]
+        app.component(iconName, item)
+    }
+}
 app.mount('#app');
